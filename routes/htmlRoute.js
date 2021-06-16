24dc40d1d5b93a7f86f5cfa10e83cc33fcac5145
LHWOOD <hutsonwood@gmail.com>
2021-06-15 21:16:10 -0600

    created the htmkroutes.js

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -2,4 +2,16 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.ger("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 module.exports = router;
