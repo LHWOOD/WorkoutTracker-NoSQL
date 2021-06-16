89eba85b8861931df014702f1634a8fe00354964
LHWOOD <hutsonwood@gmail.com>
2021-06-15 20:56:41 -0600

    created the apiroutes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,3 +1,36 @@
 const express = require("express");
+const { db } = require("../models/workout");
 const router = express.Router();
 const Workout = require("../models/workout");
+
+router.get("/api/workouts", (req, res) => {
+  db.find()
+    .then((dbData) => {
+      res.json(dbdata);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.find()
+    .then((dbdata) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.findByIdAndUpdate(params.id, { $push: { exercise: body } })
+    .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
